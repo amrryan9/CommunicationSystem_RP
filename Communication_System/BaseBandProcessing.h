5b53cfe0dfb4c3e148e2f05d5075523898ad0eaf
amr <amrryan9@gmail.com>
2021-08-12 21:44:40 +1000

    First Commit

---
+++ Communication_System/BaseBandProcessing.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include<iostream>
+
+class BaseBandProcessing
+{
+public:
+	//Input Port
+	//output Port
+public:
+
+};
---
+++ Communication_System/Source.cpp
@@ -0,0 +1,8 @@
+#include"BaseBandProcessing.h"
+#include<iostream>
+using namespace std;
+
+void main(void)
+{
+	cout << " Comminications System" << endl;
+}
